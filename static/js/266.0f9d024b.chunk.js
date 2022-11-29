97735b84f5fbd74232522ef98c00d57ba5265d6a
Dolnikova <98531699+Dolnikova@users.noreply.github.com>
2022-11-29 20:47:39 +0000

    Deploying to gh-pages from @ Dolnikova/goit-react-hw-08-phonebook@fa2addf39ee949b8e0939da85993d02487b6e9e5 🚀

---
+++ static/js/266.0f9d024b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{1266:function(e,n,i){i.r(n),i.d(n,{default:function(){return o}});var r=i(184);function o(){return(0,r.jsx)("div",{children:(0,r.jsxs)("h1",{children:["This is welcome page"," ",(0,r.jsx)("span",{role:"img","aria-label":"Greeting icon",children:"\ud83d\udc81\u200d\u2640\ufe0f"})]})})}}}]);
+//# sourceMappingURL=266.0f9d024b.chunk.js.map
